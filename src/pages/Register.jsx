f7ac8cf2b7ba7dcc38e6500d24edb19c5f4cba2c
Matt-tedesco98 <matthewtedesco3@gmail.com>
2025-10-11 16:13:55 -0400

    register page logic

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -7,7 +7,39 @@ export default function Register() {
         setForm({...form, [e.target.name]: e.target.value});
     }
 
+    function onSubmit() {
+        console.log(form)
+    }
+
     return (
-        <h1>Register</h1>
+        <div>
+            <h1>Create your account</h1>
+            <form onSubmit={(e) => {e.preventDefault(); onSubmit();}}>
+                <div>
+                    <label>Username</label> <br/>
+                    <input
+                        id="username"
+                        name="username"
+                        value={form.username}
+                        onChange={onChange}
+                        autoComplete="username"
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Password</label> <br/>
+                    <input
+                        id="password"
+                        name="password"
+                        value={form.password}
+                        onChange={onChange}
+                        autoComplete="new-password"
+                        required
+                        minLength={8}
+                    />
+                </div>
+                <button type="submit">Sign Up</button>
+            </form>
+        </div>
     )
 }
